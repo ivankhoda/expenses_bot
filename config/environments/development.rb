096fd121939bdd258f6a0a350e5912427d4896c6
Ivan Khoda <ivankhoda@gmail.com>
2022-05-22 23:04:33 +0300

    Adjusting webhooks

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -67,7 +67,7 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
-
+  config.hosts << /[a-z0-9-]+\.ngrok\.io/
   Telegram.bots_config = {
     default: ENV['test_token'],
     chat: {
