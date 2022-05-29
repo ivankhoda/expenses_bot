637056a89ac646a1c231d448ed7f3f3ac5c7e2f6
Ivan Khoda <ivankhoda@gmail.com>
2022-05-29 22:32:03 +0300

    Minor fixes

--- app/helpers/expense_helper.rb
+++ app/helpers/expense_helper.rb
@@ -14,17 +14,6 @@ module ExpenseHelper
     end
   end
 
-  def create_expense(data, user_id)
-    expense = Expense.new(data)
-    expense.user_id = user_id
-    result_message = if expense.save
-                       "Expense #{expense.id} for #{expense.category} category was created succesfully"
-                     else
-                       'Expanse was not created'
-                     end
-    respond_with :message, text: result_message
-  end
-
   def find_all_expenses_for_user(username)
     current_user = User.find_by_username(username)
     expenses = current_user.expenses.group_by(&:category).sort_by { 'category' } if current_user
