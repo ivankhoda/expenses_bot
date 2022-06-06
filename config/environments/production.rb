b54c8695549e8164cb3bbda5a9268ea7a1fa96ef
Ivan Khoda <ivankhoda@gmail.com>
2022-06-06 10:53:07 +0300

    Conf storage in prduction

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,6 +88,7 @@ Rails.application.configure do
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
   routes.default_url_options = { host: ENV['url'], protocol: 'https' }
+  config.telegram_updates_controller.session_store = :redis_store, { expires_in: 1.month }
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
